35434f890ad6b9f811b20719b03ba39b2b80cfab
CatalinBudai <alux9522853w@ieselcaminas.org>
2018-02-04 11:02:12 +0100

    Cambiamos el nombre de TestRPN a Calculadora mediante una refactorizacion

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -1,8 +1,18 @@
 package calculadora;
 import java.util.Scanner;
 
-public class TestRPN {
-	/* metodo main */
+/**
+ * <p>
+ * Clase que instancia un objeto de la clase RPN y le pasa como parametro la expresion que el usuario introduce por teclado.
+ * </p>
+ * @author MIHAI CATALIN BUDAI
+ * @since 01/01/2018
+ * @version 1.0
+ *
+ */
+
+public class Calculadora {
+	//metodo main
 	public static void main(String args[]) {
 		while(true) {
 			Scanner in = new Scanner(System.in);
